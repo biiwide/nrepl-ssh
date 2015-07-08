63487a207e8143d44a0bbadfa9e611048a6490ab
tcushman <ted@homespotter.com>
2015-07-08 13:02:57 -0500

    Updating nREPL dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.nrepl "0.2.5"]
+                 [org.clojure/tools.nrepl "0.2.10"]
                  [clj-ssh "0.5.11"]])
